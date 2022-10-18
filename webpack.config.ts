2abec6ae82ca73f7cc78e355cefe529d08a12464
RicardoKd <richardo.kadyan@gmail.com>
2022-10-18 18:00:57 +0300

    config files modified

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+
+// const __dirname = path.dirname(__filename);
+
+// export default {
+//   mode: "development",
+//   entry: "./src/index.ts",
+//   module: {
+//     rules: [
+//       {
+//         test: /\.ts?$/,
+//         use: "ts-loader",
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: [".tsx", ".ts", ".js"],
+//   },
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname, "dist"),
+//   },
+// };
+
+// import * as path from 'path';
+// import type * as webpack from 'webpack';
+// // in case you run into any typescript error when configuring `devServer`
+// import 'webpack-dev-server';
+
+// const config: webpack.Configuration = {
+//   mode: 'production',
+//   entry: './foo.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'foo.bundle.js',
+//   },
+// };
+
+// export default config;
+
+//  TODO: LOOK UP: https://webpack.js.org/configuration/configuration-languages/
+// https://webpack.js.org/guides/typescript/
+// https://learntypescript.dev/12/l4-webpack
+// https://gist.github.com/rupeshtiwari/e7235addd5f52dc3e449672c4d8b88d5
