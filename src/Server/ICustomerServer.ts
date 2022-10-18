1cad05b056f0edae397e26faf308a593eaabad4e
RicardoKd <richardo.kadyan@gmail.com>
2022-10-18 18:02:58 +0300

    rentCells method signature updated

--- src/Server/ICustomerServer.ts
+++ src/Server/ICustomerServer.ts
@@ -3,7 +3,11 @@ import type { Types } from "mongoose";
 export default interface ICustomerServer {
   signUp(name: string, email: string, password: string): Promise<boolean>;
   logIn(email: string, password: string): Promise<boolean>;
-  rentCells(quantityOfCells: number, ownerId: Types.ObjectId): Promise<boolean>;
+  rentCells(
+    quantityOfCells: number,
+    rentEndDate: Date,
+    ownerId: Types.ObjectId,
+  ): Promise<boolean>;
   getCellContent(
     cellsIds: number[],
     customerId: Types.ObjectId,
